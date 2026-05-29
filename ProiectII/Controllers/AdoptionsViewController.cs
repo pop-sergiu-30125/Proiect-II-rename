796f758b9f1c71373207f0cfd56543e7492e0e70
moldovan-radu-30125 <Moldovan.Vi.Radu@student.utcluj.ro>
2026-05-29 09:49:57 +0300

    added manage adoptions page

---
+++ ProiectII/Controllers/AdoptionsViewController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProiectII.Controllers
+{
+    [Authorize(Roles = "Admin,Employee")]
+    public class AdoptionsViewController : Controller
+    {
+        [HttpGet]
+        public IActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
